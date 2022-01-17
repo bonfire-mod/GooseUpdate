286f488a8aec6291b9170b919e8a50c9de1a7652
Steviegt6 <xxlennygamerxx@gmail.com>
2022-01-17 00:54:36 -0800

    B

--- branches/mod/bonfire/patch.js
+++ branches/mod/bonfire/patch.js
@@ -1,7 +1,7 @@
 (async function() {
   const rgb = (r, g, b, text) => `\x1b[38;2;${r};${g};${b}m${text}\x1b[0m`;
 
-  const log = function() { console.log(`[${rgb(250, 250, 0, 'bonfire')}]`, ...arguments); }
+  const log = function() { console.log(`[${rgb(250, 250, 0, 'Bonfire')}]`, ...arguments); }
 
   const electron = require('electron');
 
